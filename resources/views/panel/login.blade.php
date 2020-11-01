dca4bc1c2d1d65451476069d84f0b821fa33117a
yakupdemircia <yakup@engineer.com>
2020-11-01 16:19:44 +0300

    login screens

--- resources/views/panel/login.blade.php
+++ resources/views/panel/login.blade.php
@@ -6,11 +6,11 @@
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="inputGroup inputGroup1">
             <label for="loginEmail" id="loginEmailLabel">Email</label>
-            <input name="email" type="email" id="loginEmail" maxlength="254"/>
+            <input name="email" type="email" id="loginEmail" maxlength="254" value="admin@mylibrary.com"/>
         </div>
         <div class="inputGroup inputGroup2">
             <label for="loginPassword" id="loginPasswordLabel">Password</label>
-            <input name="password" type="password" id="loginPassword"/>
+            <input name="password" type="password" id="loginPassword" value="132435"/>
         </div>
         <div class="inputGroup inputGroup3">
             <button id="login">Log in</button>
